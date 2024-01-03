484d2280e12825b09b13e4e328b494fd72abf66b
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-03 22:08:29 +0800

    Add: error modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import routes from "./routes";
+import ErrorModal from "./components/ErrorModal";
 
 function App() {
   return (
     <>
+      <ErrorModal />
       <Router>
         <Routes>
           {routes.map((route, i) => (
