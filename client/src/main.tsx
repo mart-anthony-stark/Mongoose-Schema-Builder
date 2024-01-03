1fe6203d7bd72a4d6794d86099a2eb2d3bcc6cda
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-03 20:36:25 +0800

    Add: Models Context Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ModelsContextProvider } from "./context/ModelsContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ModelsContextProvider>
     <App />
-  </React.StrictMode>,
-)
+  </ModelsContextProvider>
+);
