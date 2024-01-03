1a0eb08309b4e58c3375c4e6e419f5c695561b7c
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-03 21:06:03 +0800

    Add: selected model

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,14 +2,16 @@ import AppHeader from "../../components/AppHeader/AppHeader";
 import ModelsSidebar from "./ModelsSidebar/ModelsSidebar";
 import "./home.css";
 import AttributeSidebar from "./AttributeSidebar/AttributeSidebar";
+import { useModelsContext } from "../../hooks/useModelsContext";
 
 function index() {
+  const { selectedModel } = useModelsContext();
   return (
     <div className="h-[100vh]">
       <AppHeader />
       <div className="flex justify-between h-full pt-16">
         <ModelsSidebar />
-        <main></main>
+        <main>{JSON.stringify(selectedModel)}</main>
         <AttributeSidebar />
       </div>
     </div>
