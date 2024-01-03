54efda876c25583b99308e113851b0d46c5f8103
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-03 20:37:16 +0800

    Add: Models Map

--- client/src/pages/Home/ModelsSidebar/ModelsSidebar.tsx
+++ client/src/pages/Home/ModelsSidebar/ModelsSidebar.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import Sidebar from "../../../components/Sidebar/Sidebar";
+import { useModelsContext } from "../../../hooks/useModelsContext";
+import { TbFileDatabase } from "react-icons/tb";
 
-const ModelsSidebar: React.FC = () => (
-  <Sidebar className="border-r-[1px]">
-    <Sidebar.Title title="Models">
-      <button className="btn btn-sm">Add Model</button>
-    </Sidebar.Title>
+import { IoLogoJavascript } from "react-icons/io5";
+import { IconContext } from "react-icons";
 
-    <Sidebar.Scroller></Sidebar.Scroller>
-  </Sidebar>
-);
+const ModelsSidebar: React.FC = () => {
+  const { models } = useModelsContext();
+  return (
+    <Sidebar className="border-r-[1px]">
+      <Sidebar.Title title="Models">
+        <TbFileDatabase size={30} />
+      </Sidebar.Title>
+
+      <Sidebar.Scroller
+        data={models}
+        keyExtractor="_id"
+        RenderItem={(item: any) => (
+          <div className="flex items-center gap-2">
+            <IconContext.Provider value={{ className: "text-yellow-6  00" }}>
+              <IoLogoJavascript size={20} />
+            </IconContext.Provider>
+            <div>{item.name}.model.js</div>
+          </div>
+        )}
+      />
+    </Sidebar>
+  );
+};
 
 export default ModelsSidebar;
