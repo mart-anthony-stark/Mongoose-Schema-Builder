9ca82589ca40bb8a9856babea8722dd207218ec5
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-04 17:53:45 +0800

    Add: export model

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,21 +1,25 @@
 import { Model } from "../types";
 
 export const generateCode = (model: Model): string => {
-  let output = `
-const mongoose = require("mongoose");
+  const modelName =
+    model.name?.charAt(0).toUpperCase() + model.name?.slice(1).toLowerCase();
+  let output = `const mongoose = require("mongoose");
 
 const ${model.name?.toLowerCase()}Schema = new mongoose.Schema(
     {
-    ${model.attributes
-      ?.map((attr: any) => {
-        return `${attr.name}: {
+\t\t${model.attributes
+    ?.map((attr: any) => {
+      return `${attr.name}: {
             type: ${attr.type}
         }`;
-      })
-      .join(",\n\t\t")}
+    })
+    .join(",\n\t\t")}
     }
 );
-    `;
+
+const ${modelName} = mongoose.model("${model.name?.toLowerCase()}Schema", ${model.name?.toLowerCase()}Schema);
+module.exports = ${modelName};
+`;
 
   return output;
 };
