fdb663ae42d1739b6131d6d124294aeaf87cb5d9
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-04 14:10:19 +0800

    Add: code viewer

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,16 +2,15 @@ import AppHeader from "../../components/AppHeader";
 import ModelsSidebar from "./ModelsSidebar";
 import "./home.css";
 import AttributeSidebar from "./AttributeSidebar";
-import { useModelsContext } from "../../hooks/useModelsContext";
+import CodeViewer from "./CodeViewer";
 
 function index() {
-  const { selectedModel } = useModelsContext();
   return (
     <div className="h-[100vh]">
       <AppHeader />
       <div className="grid grid-cols-[200px_1fr_300px] h-full pt-16">
         <ModelsSidebar />
-        <main className="flex-1 w-full">{JSON.stringify(selectedModel)}</main>
+        <CodeViewer />
         <AttributeSidebar />
       </div>
     </div>
