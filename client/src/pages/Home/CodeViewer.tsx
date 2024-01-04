2fa29de24202353dc42030640ead5b3f8ca733ce
mart-anthony-stark <martsalazar0@gmail.com>
2024-01-04 17:53:22 +0800

    Add: copy to clipboard

--- client/src/pages/Home/CodeViewer.tsx
+++ client/src/pages/Home/CodeViewer.tsx
@@ -14,20 +14,42 @@ SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 function CodeViewer() {
   const { selectedModel } = useModelsContext();
+  const [toastVisible, setToastVisibility] = useState(false);
   const [code, setCode] = useState("");
 
   useEffect(() => {
     if (selectedModel) setCode(generateCode(selectedModel));
   }, [selectedModel]);
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(code);
+    setToastVisibility(true);
+    setTimeout(() => {
+      setToastVisibility(false);
+    }, 1000);
+  };
+
   return (
     <main className="flex-1 w-full">
+      {toastVisible && (
+        <div className="toast toast-top toast-center">
+          <div className="alert alert-success">
+            <span>Copied to clipboard.</span>
+          </div>
+        </div>
+      )}
+      <nav></nav>
       {!selectedModel ? (
-        <h1 className="text-center text-gray-600 text-3xl mt-64">
-          No selected
+        <h1 className="text-center text-gray-600 text-3xl mt-64 select-none">
+          No selected model
         </h1>
       ) : (
         <div className="code-viewer h-full">
+          <div className="flex justify-end p-2">
+            <button onClick={handleCopy} className="btn">
+              Copy
+            </button>
+          </div>
           <SyntaxHighlighter language="js" style={{ ...darcula }}>
             {code}
           </SyntaxHighlighter>
